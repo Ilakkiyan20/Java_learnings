398edd8dfcde1572a50b6d12051416f3c21facb4
[plakiopo74-bot] <[plakiopo74@gmail.com]>
2026-09-21 16:18:31 +0530

    Day1

--- ControlStructures.java
+++ ControlStructures.java
@@ -1,5 +1,10 @@
 public class ControlStructures {
     public static void main (String []args){
-        
+    inht a=10;
+    int b=20;
+    if (a>b){
+        system.out.println("a is greater");
+        else
+    }
     }
 }
--- Exercise.java
+++ Exercise.java
@@ -8,5 +8,20 @@ public class Exercise {
     System.out.println("int"+age);
     System.out.println("float"+height);
     System.out.println("double"+weight);
+
+    //second exercise 
+    //create integer variables a=15 and b=25.print their sum,difference,product,divison
+    int a=15;
+    int b=25;
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+
+    //third exercise
+    //create a string variable for your city name and an int variable for pin code.print both
+    String cityname="thiruvallur";
+    int pincode=602001;
+    System.out.println(cityname+pincode);
     }
 }
